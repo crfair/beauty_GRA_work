55c915faa31910a9863ae5527ca52dcf23100a94
Corinne Fair <cfair.5298@gmail.com>
2021-09-10 13:45:34 -0400

    Meshes are working out pretty well; need to adjust threshold and keep reading materials. background subtraction combined with this could work well. also might consider comparing lightess of current pixel with that of those around it

--- swarming_meshes/src/ofApp.cpp
+++ swarming_meshes/src/ofApp.cpp
@@ -3,27 +3,29 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 	image.load("bacteria.jpg"); // load in image from bin/data
-	background.load("bacteria_background.jpg");
+	image.resize(360, 225);
 
-	threshold = 60;
+	//background.load("bacteria_background.jpg");
 
-	colorImg.allocate(1440, 900);
-	grayImg.allocate(1440, 900);
-	grayBg.allocate(1440, 900);
-	grayDiff.allocate(1440, 900);
+	//threshold = 60;
 
-	colorImg = image;
-	grayImg = colorImg;
-	grayBg = background;
-	grayDiff.absDiff(grayBg, grayImg);
-	grayDiff.threshold(threshold);
+	//colorImg.allocate(1440, 900);
+	//grayImg.allocate(1440, 900);
+	//grayBg.allocate(1440, 900);
+	//grayDiff.allocate(1440, 900);
 
-	contourFinder.findContours(grayDiff, 20, (1440 * 900) / 3, 10, true);
+	//colorImg = image;
+	//grayImg = colorImg;
+	//grayBg = background;
+	//grayDiff.absDiff(grayBg, grayImg);
+	//grayDiff.threshold(threshold);
+
+	//contourFinder.findContours(grayDiff, 20, (1440 * 900) / 3, 10, true);
 
 	mesh.setMode(OF_PRIMITIVE_LINES); // set mode to create points only
 	mesh.enableColors();
 
-	float intensityThreshold = 200.0;
+	float intensityThreshold = 125.0;
 	int w = image.getWidth();
 	int h = image.getHeight();
 
@@ -33,9 +35,23 @@ void ofApp::setup(){
 			ofColor c = image.getColor(x, y); // get the pixel's color
 			float intensity = c.getLightness(); // and it's lightness
 			if (intensity >= intensityThreshold) { // if the pixel is sufficiently light...
-				ofVec3f pos(x, y, 0.0); // add it to the mesh
+				ofVec3f pos(x*4, y*4, 0.0); // add it to the mesh
 				mesh.addVertex(pos);
 				mesh.addColor(c); // the mesh will autoconvert the ofColor to an ofFloatColor
+			}
+		}
+	}
+
+	float connectionDistance = 30;
+	int numVerts = mesh.getNumVertices();
+	for (int a = 0; a < numVerts; ++a) {
+		ofVec3f verta = mesh.getVertex(a);
+		for (int b = a + 1; b < numVerts; ++b) {
+			ofVec3f vertb = mesh.getVertex(b);
+			float distance = verta.distance(vertb);
+			if (distance <= connectionDistance) {
+				mesh.addIndex(a);
+				mesh.addIndex(b);
 			}
 		}
 	}
@@ -48,22 +64,25 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofSetHexColor(0xffffff);
-	ofFill();
-	ofSetHexColor(0x333333);
-	ofDrawRectangle(0, 0, 1440, 900);
-	ofSetHexColor(0xffffff);
+	//ofSetHexColor(0xffffff);
+	//ofFill();
+	//ofSetHexColor(0x333333);
+	//ofDrawRectangle(0, 0, 1440, 900);
+	//ofSetHexColor(0xffffff);
 
-	for (int i = 0; i < contourFinder.nBlobs; i++) {
-		contourFinder.blobs[i].draw(0, 0);
-		ofSetColor(255);
-		if (contourFinder.blobs[i].hole) {
-			ofDrawBitmapString("hole",
-				contourFinder.blobs[i].boundingRect.getCenter().x,
-				contourFinder.blobs[i].boundingRect.getCenter().y);
-		}
-	}
+	//for (int i = 0; i < contourFinder.nBlobs; i++) {
+	//	contourFinder.blobs[i].draw(0, 0);
+	//	ofSetColor(255);
+	//	if (contourFinder.blobs[i].hole) {
+	//		ofDrawBitmapString("hole",
+	//			contourFinder.blobs[i].boundingRect.getCenter().x,
+	//			contourFinder.blobs[i].boundingRect.getCenter().y);
+	//	}
+	//}
 
+	ofColor centerColor = ofColor(85, 78, 68);
+	ofColor edgeColor(0, 0, 0);
+	ofBackgroundGradient(centerColor, edgeColor, OF_GRADIENT_CIRCULAR);
 	mesh.draw();
 }
 
